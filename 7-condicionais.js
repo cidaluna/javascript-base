54f6c6c7bb63ad49c7968fcccc40276a143f86fa
Cida Luna <cidalunna@hotmail.com>
2024-11-15 10:40:38 -0300

    refactor conditional and logical operators

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -10,22 +10,51 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 15;
+const estaAcompanhada = true;
+const temPassagem = true;
+
+console.log("Destinos possíveis: ", listaDeDestinos);
 
 // Condicional (if else) se comprador for maior de idade,
 // então pode exibir a lista de destinos
 // e pode remover da lista o destino que quiser
-if(idadeComprador >= 18){
-    console.log("Comprador maior de idade!");
-    console.log(listaDeDestinos);
-    // remover Rio de Janeiro da lista
+// if(idadeComprador >= 18){
+//     console.log("Comprador maior de idade!");
+//     console.log(listaDeDestinos);
+//     // remover Rio de Janeiro da lista
+//     listaDeDestinos.splice(2,1);
+//     console.log(listaDeDestinos);
+//     }else if(estaAcompanhada){
+//         // Menor acompanhado
+//         console.log("Comprador está acompanhado!");
+//         // remover Rio de Janeiro da lista
+//         listaDeDestinos.splice(2,1);
+//         console.log(listaDeDestinos);
+//     }else{
+//         console.log("Menor e não está acompanhado!!");
+// }
+
+
+// bloco refatorado
+if(idadeComprador >= 18 || estaAcompanhada == true){
+    console.log("Boa viagem!"); // Comprador maior de idade OU está acompanhado!
     listaDeDestinos.splice(2,1);
     console.log(listaDeDestinos);
-}else{
-    console.log("Não é maior de idade, não pode acessar a lista.")
+    }else{
+        console.log("Menor e não está acompanhado!!");
 }
 
-console.log(idadeComprador > 18);  // maior do que 18? retorno false 15 é menor
-console.log(idadeComprador < 18);  // menor do que 18? true 15 é menor
-console.log(idadeComprador >= 18);  // maior ou igual a 18? false
-console.log(idadeComprador <= 18);  // menor ou igual a 18? true 15 é menor
-console.log(idadeComprador == 18);  //é igual a 18? false 15 é menor
+console.log("Embarque: \n ");  // o uso de \n para quebrar linha
+if((idadeComprador >= 18 && temPassagem) || (temPassagem && estaAcompanhada)){
+    console.log("Liberado!");
+}else{
+    console.log("Embarque não permitido!");
+}
+
+// ctrl k + ctrl c para comentar
+// ctrl k + ctrl u para descomentar
+// console.log(idadeComprador > 18);  // maior do que 18? retorno false 15 é menor
+// console.log(idadeComprador < 18);  // menor do que 18? true 15 é menor
+// console.log(idadeComprador >= 18);  // maior ou igual a 18? false
+// console.log(idadeComprador <= 18);  // menor ou igual a 18? true 15 é menor
+// console.log(idadeComprador == 18);  //é igual a 18? false 15 é menor
