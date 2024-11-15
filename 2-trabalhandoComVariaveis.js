4a202dbd2492c2fc17696d0faef5bfd6ea95a524
Cida Luna <cidalunna@hotmail.com>
2024-11-15 10:00:02 -0300

    variables const and let

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,7 @@
+console.log("Trabalhando com variáveis:"); // JS é case sensitive isso funciona
+// Console.log("trabalhando com variáveis"); // JS é case sensitive isso não funciona
+
+const nome = "Cida";  // sempre declarar o tipo da variável para ela não subir para o escopo global.
+let ano = 2024;
+console.log(nome);
+console.log(ano);
