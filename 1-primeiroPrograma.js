9cf3ee8c9abbb918ffef37aa932739f1e286255b
Cida Luna <cidalunna@hotmail.com>
2024-11-15 09:58:09 -0300

    iniciando console log JS

---
+++ 1-primeiroPrograma.js
@@ -0,0 +1,9 @@
+console.log("Meu primeiro programa JS: trabalhando com variáveis"); // executar no terminal "node .\1-primeiroPrograma.js"
+const idade = 36;  // declaração de uma const
+console.log(idade,"anos.");
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+1; 
+console.log(`Idade somanda: ${idadeSomada} anos.`);
