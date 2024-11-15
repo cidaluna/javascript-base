2ccdd0ef8717d10be951ec2cfe4715e3c0d38b96
Cida Luna <cidalunna@hotmail.com>
2024-11-15 10:02:56 -0300

    Add arithmetic operators

---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,8 @@
+console.log("Operações Aritméticas:");
+
+console.log(2+2);
+console.log(10 + 8 * 2);  // retorna 26 pois multiplica primeiro
+console.log((10 + 8) * 2);  // retorna 36 pois resolve o que esta dentro dos parenteses primeiro
+
+console.log("ano " + 2024); // o JS junta o resultado, ou seja, concatena string com o numero
+console.log("2" + "2"); // concatena e resulta em 22 pois o JS junta as strings
