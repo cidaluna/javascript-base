f76b3fc5e761c8e6a256f200f4ea74d652c39e30
Cida Luna <cidalunna@hotmail.com>
2025-01-19 15:31:57 -0300

    Add some exercises with matriz in JS

--- 6-5-exercicios.js
+++ 6-5-exercicios.js
@@ -23,3 +23,29 @@ const menuSobremesas = ["Bolo de chocolate", "Pudim", "Pavê", "Sorvete", "Salad
 const menuCompleto = menuPrincipal.concat(menuSobremesas);
 console.log(menuCompleto);
 
+/*5 - Crie uma lista bidimensional com 3 linhas e 3 colunas, onde cada elemento seja uma matriz 3x3 com valores iniciando em 1 e aumentando em 1 a cada elemento.
+Dicas:
+    comece com um array vazio, por exemplo const matriz = [] e adicione valores nele com push;
+    você pode resolver usando um for dentro de outro for.
+*/
+let matriz = [];
+let valorInicial = 1;
+
+for (let i = 0; i < 3; i++) {
+  let linha = [];
+  for (let j = 0; j < 3; j++) {
+    linha.push(valorInicial++);
+  }
+  matriz.push(linha);
+}
+
+console.log('Matriz de duas dimensões:');
+console.log(matriz);
+matriz.forEach(row => console.log(row)); // visualização em linhas e colunas
+
+//6-Acesse e imprima o elemento na segunda linha e terceira coluna da lista bidimensional matriz criada no exercício anterior.
+console.log(matriz[1][2]);
+
+//7-Adicione um novo elemento (por exemplo,15) na terceira linha e segunda coluna da lista bidimensional matriz criada anteriormente.
+matriz[2][1] = 15;
+console.log(matriz);
