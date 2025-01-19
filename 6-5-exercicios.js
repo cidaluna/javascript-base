42b4a0ee78dfcf5eae526915729daeaee924d756
Cida Luna <cidalunna@hotmail.com>
2025-01-19 19:37:28 -0300

    Add some exercises in JS

--- 6-5-exercicios.js
+++ 6-5-exercicios.js
@@ -78,8 +78,7 @@ for(let i=0;i<notas.length;i++){
   console.log(`Índice: ${i}, nota: ${notas[i]}`);
 }
 
-//10-Somar as notas do array
-
+//10-Somar as notas do array com o for...of
 const notasMes = [10,10,2,5,7,5.5];
 let somaDasNotas = 0;
 
@@ -89,3 +88,70 @@ for(const element of notasMes){
 console.log(somaDasNotas);
 const mediaNotas = somaDasNotas / notasMes.length;
 console.log(`A média das notas é ${mediaNotas.toFixed(2)}.`);
+
+//11-Somar as notas do array com o forEach
+const notasNew = [10,10,2,5,7,5.5];
+let somaNotas = 0;
+
+notasNew.forEach(function (nota){
+  somaNotas += nota;
+})
+
+console.log(somaNotas);
+const mediaN = somaNotas / notasNew.length;
+console.log(`A média das notas é ${mediaN.toFixed(2)}.`);
+
+//12-Adicionar 1 ponto em cada nota do aluno, utilizando o metodo map()
+const n = [10, 9.5, 8, 7, 6];
+
+// garantir que não tenha nota maior que 10
+const nAtualizadas = n.map((nota) => nota + 1 >= 10 ? 10 : nota+1);
+
+console.log(nAtualizadas);
+
+/**
+ * Enquanto o método forEach não retorna nenhum tipo de dados, o método map() sempre retorna um array. Caso a função callback não tenha nenhum dado de retorno, ainda assim o resultado de map() será um array vazio. Por isso qualquer tentativa de retornar resultados de forEach e armazená-los em variáveis vai resultar em undefined.
+ */
+
+//13-Padronizar os nomes para que todos estejam com letras maiusculas utilizando o map()
+const nomes = ["ana Julia", "Caio viNicius", "CIDA lUna", "Bia GoncalveS"];
+const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
+console.log(nomesPadronizados);
+
+//14-Imprimir cada elemento do array
+const arrayA = ['a', 'b', 'c'];
+arrayA.forEach((element) => console.log(element));
+
+const meuArray = ['a', 'b', 'c', 'd', 'e'];
+meuArray.forEach((elemento, indice) => {
+  console.log(`Índice: ${indice}, Valor: ${elemento}`);
+});
+
+//15-Crie uma função chamada executaOperacaoEmArray que recebe dois parâmetros: um array e uma função de callback que executa alguma operação matemática. Essa função deve iterar por cada elemento do array e aplicar a função de callback em cada um dos elementos, imprimindo o resultado da operação no console.
+
+function executaOperacaoEmArray(array, funcaoCallback) {
+    return array.map(funcaoCallback); // Executa a função de callback em cada elemento do array
+}
+
+function dobraNumero(num) {
+    return num * 2; // Função de exemplo para dobrar o número
+}
+
+const listaNumeros = [1, 2, 3];
+const listaNumerosDobrados = executaOperacaoEmArray(listaNumeros, dobraNumero);
+console.log(listaNumerosDobrados); 
+
+//16-Você recebeu um array numeros contendo valores numéricos. Crie um programa que verifique se um número específico está presente nesse array. Se estiver, o programa deve retornar a posição (índice) desse número. Caso contrário, se o número não estiver presente, o programa deve retornar "-1".
+
+const numerosA = [10, 20, 30, 40, 50];
+const numeroProcurado = 30;
+let posicao = -1;
+
+for (let i = 0; i < numerosA.length; i++) {
+  if (numerosA[i] === numeroProcurado) {
+    posicao = i;
+    break;
+  }
+}
+
+console.log(`Posição do número ${numeroProcurado}: ${posicao}`);
